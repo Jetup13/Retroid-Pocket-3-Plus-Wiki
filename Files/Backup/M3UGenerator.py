62d1cafa71b9288c0c6878a808381bc6877d9356
Jetup13 <62556437+Jetup13@users.noreply.github.com>
2023-12-11 12:22:46 -0800

    Update M3UGenerator.py

--- Files/Backup/M3UGenerator.py
+++ Files/Backup/M3UGenerator.py
@@ -1,50 +1,30 @@
 import os
 
-def create_m3u_playlists(folder_path):
-    files = [file for file in os.listdir(folder_path) if (file.endswith('.chd') or file.endswith('.cue')) and '(Disc' in file]
+def generate_m3u(directory):
+    files_by_base = {}
 
-    path_prefix = input("Enter the path prefix (press Enter for no prefix): ").strip()
+    for filename in os.listdir(directory):
+        if "(Disc" in filename or "(Disk" in filename:
+            base_name = os.path.splitext(os.path.basename(filename))[0]
+            base_name = base_name.split("(Disc")[0].strip()  # Remove "(Disc" and trim spaces
+            base_name = base_name.split("(Disk")[0].strip()  # Remove "(Disk" and trim spaces
 
-    name_groups = {}
-    for file in files:
-        base_name, region_info = get_base_name_and_region(file)
-        key = (base_name, region_info)
-        if key not in name_groups:
-            name_groups[key] = []
-        name_groups[key].append(os.path.join(folder_path, file))
+            if base_name not in files_by_base:
+                files_by_base[base_name] = []
+            files_by_base[base_name].append(filename)
 
-    for (base_name, region_info), file_paths in name_groups.items():
-        create_m3u_playlist(file_paths, base_name, region_info, path_prefix)
+    prefix = input("Enter prefix to add to the beginning of each line or press ENTER for no prefix: ")
 
-def get_base_name_and_region(file_name):
-    # Extract the base name and region information
-    # Modify this function based on your file naming convention
-    parts = file_name.split(' ')
-    base_name_parts = []
-    region_info_parts = []
+    for base_name, file_list in files_by_base.items():
+        m3u_name = f"{base_name}.m3u"
+        with open(m3u_name, 'w') as m3u_file:
+            lines = [prefix + filename for filename in file_list]
+            lines = [line for line in lines if line.strip()]  # Remove empty lines
+            m3u_file.write("\n".join(lines))
+        print(f"M3U file '{m3u_name}' generated with {len(file_list)} files.")
 
-    for part in parts:
-        if part.startswith('(Disc') or part.startswith('(Rev'):
-            break
-        if part.startswith('('):
-            region_info_parts.append(part)
-        else:
-            base_name_parts.append(part)
+# Get the directory of the script
+script_directory = os.path.dirname(os.path.abspath(__file__))
 
-    base_name = ' '.join(base_name_parts)
-    region_info = ' '.join(region_info_parts)
-    
-    return base_name, region_info
-
-def create_m3u_playlist(file_paths, base_name, region_info, path_prefix):
-    m3u_name = f'{base_name} {region_info}'.strip() if region_info else base_name
-    lines = [path_prefix + os.path.basename(path) for path in file_paths]
-
-    with open(m3u_name + '.m3u', 'w') as playlist_file:
-        playlist_file.write('\n'.join(lines))
-
-# Get the current script's directory
-script_directory = os.path.dirname(os.path.realpath(__file__))
-
-# Example usage:
-create_m3u_playlists(script_directory)
+# Use the script directory as the base directory
+generate_m3u(script_directory)
