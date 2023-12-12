80803a4fcc3e147c4ecdc9e90eba14c90f7e5fb0
Jetup13 <62556437+Jetup13@users.noreply.github.com>
2023-12-11 20:29:56 -0800

    Update M3UGenerator.py

--- Files/Backup/M3UGenerator.py
+++ Files/Backup/M3UGenerator.py
@@ -9,25 +9,42 @@ def generate_m3u(directory):
             base_name = base_name.split("(Disc")[0].strip()  # Remove "(Disc" and trim spaces
             base_name = base_name.split("(Disk")[0].strip()  # Remove "(Disk" and trim spaces
 
+            # Extract revision number
+            rev_start = filename.find("(Rev")
+            if rev_start != -1:
+                rev_end = filename.find(")", rev_start)
+                if rev_end != -1:
+                    revision = filename[rev_start:rev_end + 1].strip()
+                    base_name += f" {revision}"
+
+            if "(Rev" in filename:
+                if "(Disc" not in filename:
+                    continue  # Skip files with "(Rev" but without "(Disc"
+                
             if base_name not in files_by_base:
-                files_by_base[base_name] = []
-            files_by_base[base_name].append(filename)
+                files_by_base[base_name] = set()
+
+            files_by_base[base_name].add(filename)
 
     prefix = input("Enter prefix to add to the beginning of each line (press enter for no prefix): ")
 
     created_files = []
 
-    for base_name, file_list in files_by_base.items():
+    for base_name, filenames in files_by_base.items():
         m3u_name = f"{base_name}.m3u"
         with open(m3u_name, 'w') as m3u_file:
-            lines = [prefix + filename for filename in file_list]
+            lines = [prefix + filename for filename in sorted(filenames)]
             lines = [line.strip() for line in lines if line.strip()]  # Remove empty lines
-            m3u_file.write("\n".join(lines))
-            created_files.append(m3u_name)
+            if lines:  # Only create M3U file if there are non-empty lines
+                m3u_file.write("\n".join(lines))
+                created_files.append(m3u_name)
 
-    print("Files created:")
-    for created_file in created_files:
-        print(created_file)
+    if created_files:
+        print("Files created:")
+        for created_file in created_files:
+            print(created_file)
+    else:
+        print("No files matching the criteria found.")
 
     input("Press Enter to close the dialog.")
 
